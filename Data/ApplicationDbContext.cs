8de42cd4fe249d320940e1cc81af3af3ee5a3583
Kaja Więckowska <k.wieckowska66@gmail.com>
2023-10-10 23:13:42 +0200

    Connected to database and created table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
 namespace ToDoList.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<ToDo> ToDos { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ Models/ToDo.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
