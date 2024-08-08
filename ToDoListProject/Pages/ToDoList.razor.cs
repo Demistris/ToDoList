8451ad3958d838619229c0fb4e09af6a922908cf
Kaja Więckowska <k.wieckowska66@gmail.com>
2024-08-08 20:23:43 +0200

    Add project files.

---
+++ ToDoListProject/Pages/ToDoList.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoListProject.Pages
+{
+    public partial class ToDoList
+    {
+        private List<string> _toDoList = new List<string>() { "qwe" };
+        private string _newToDoItem = "";
+
+        private void AddNewItem()
+        {
+            _toDoList.Add(_newToDoItem);
+            _newToDoItem = "";
+        }
+    }
+}
