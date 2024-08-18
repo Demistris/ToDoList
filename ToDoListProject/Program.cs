296df89030a3a3a2c6e3b089988cf1811f3793b6
Kaja Więckowska <k.wieckowska66@gmail.com>
2024-08-18 14:02:13 +0200

    Added Blazorise

--- ToDoListProject/Program.cs
+++ ToDoListProject/Program.cs
@@ -1,9 +1,20 @@
+using Blazorise;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ToDoListProject;
 using ToDoListProject.Pages;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddBlazorise(options =>
+{
+    options.Immediate = true;
+})
+.AddBootstrapProviders()
+.AddFontAwesomeIcons();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
